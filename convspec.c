98d6d1cfa790882d3aa7ef60d77a75bab092bd3c
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-10-17 04:44:20 -0700

    I'm here and I'm a Spur for life

--- convspec.c
+++ convspec.c
@@ -1,65 +1,91 @@
 #include <stdio.h>
 #include <stdarg.h>
+
+/**
+ * print_character - Print a character to stdout
+ * @c: The character to print
+ *
+ * Return: 1 for the printed character
+ */
+
+
+static int print_character(int c)
+{
+	putchar(c);
+	return (1);
+}
+
+/**
+ * handle_format - Handle the format string and print formatted output
+ * @format: The format string
+ * @args: The va_list of arguments
+ *
+ * Return: The number of characters printed (excluding the null byte)
+ */
+
+static int handle_format(const char *format, va_list args)
+{
+	int char_count;
+
+	char_count = 0;
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			char_count += print_character(*format);
+		}
+		else
+		{
+			format++;
+			if (*format == 'c')
+			{
+				int c = va_arg(args, int);
+
+				char_count += print_character(c);
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(args, char *);
+
+				while (*s)
+				{
+					char_count += print_character(*s);
+					s++;
+				}
+			} else if (*format == '%')
+			{
+				char_count += print_character('%');
+			}
+			else
+			{
+				char_count += print_character('%');
+				char_count += print_character(*format);
+			}
+		}
+		format++;
+	}
+	return (char_count);
+}
+
 /**
  * _printf - Custom printf function
  * @format: The format string
  *
- * Description: This function prints formatted output to stdout. It handles
- * the 'c' conversion specifier.
- *
- * Return: The number of characters printed (excluding the null byte).
+ * Return: The number of characters printed (excluding the null byte)
  */
+
 int _printf(const char *format, ...)
 {
-	 va_list args;
-	 int char_count;
-	 int i;
-	 int j;
+	va_list args;
+	int char_count;
 
-	 char_count = 0;
-	 va_start(args, format);
+	va_start(args, format);
 
-	 for (i = 0; format[i] != '\0'; i++)
-	 {
-		 if (format[i] != '%')
-		 {
-			 putchar(format[i]);
-			 char_count++;
-		 }
-		 else
-		 {
-			 i++;
-			 switch (format[i])
-			 {
-				 case 'c':
-					 {
-						 int c = va_arg(args, int);
-						 putchar(c);
-						 char_count++;
-					 }
-					 break;
-				 case 's':
-					 {
-						 char *s = va_arg(args, char *);
-						 for (j = 0; s[j] != '\0'; j++)
-						 {
-							 putchar(s[j]);
-							 char_count++;
-						 }
-					 }
-					 break;
-				 case '%':
-					 putchar('%');
-					 char_count++;
-					 break;
-				 default:
-					 putchar('%');
-					 putchar(format[i]);
-					 char_count += 2;
-					 break;
-			 }
-		 }
-	 }
-		 va_end(args);
-		 return (char_count);
+	char_count = handle_format(format, args);
+
+	va_end(args);
+
+	return (char_count);
 }
+
